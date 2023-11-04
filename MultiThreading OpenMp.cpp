5a2f93e2dd2f685543029c6d05ce8b9285d4f09f
Vinícius Costa <84977966+vinicosta-p@users.noreply.github.com>
2023-11-04 11:19:07 -0300

    Adição de um map com index de coluna e cometários

--- MultiThreading OpenMp.cpp
+++ MultiThreading OpenMp.cpp
@@ -1,3 +1,28 @@
+/*
+* Esse código trocará os dados categóricos por dados id e criará um dataset final com a troca
+* Ele irá criar arquivos com os dados categóricos e o códigos deles, um para cada coluna;
+* formato do dicionário: id,nome\n
+* A forma como esse código faz essa troca tem a seguinte estrutura:
+*           
+*           LOOP -> acaba quando não houver mais linhas para ler
+*           {
+*               1.(Thread Única) lê o dataset e armazena em uma matriz com um tamanho x(a definir) de linhas e 26 colunas
+*               2. distribui as threads para que elas 
+*                   -> criem os arquivos coluna.csv
+*                   -> leem a matriz, verificam se o valor lido está no arquivo ** SE NÃO ESTIVER insere o valor no arquivo ** e troca a string da matriz para o id
+*                   -> esperam as outras threads
+*               3.(Thread Única) escreve o arquivo final
+* 
+*           Recursos criados:
+*               var de nome das colunas 
+*               dicionário com o valor das index da coluna dos dados categóricos
+* 
+*           NOTAS:
+*               Pensar nesse loop de forma individual(prioridade 1)
+*               Pensar na busca de arquivo das threads(prioridade 2)
+*/
+
+
 #include <omp.h>  
 #include <stdio.h>
 #include <stdlib.h> 
@@ -12,8 +37,10 @@ using namespace std;
 
 fstream mainDataset;
 fstream finalDataset;
-
 vector<vector<string>> matrizDeDados;
+map<string, int> idxColuna;
+
+void criarMapComNomeDaColunaAndPosicao();
 
 int atualizarDataSet();
 
@@ -21,10 +48,16 @@ void escrita_do_dataset(vector<vector<string>> escritaMatriz);
 
 void pairCodigoDescricao(string nomeArquivo);
 
+vector<string> nomesArquivos;
+
 int main(int argc, char* argv[])
 {
     auto start = std::chrono::steady_clock::now();
+
+    nomesArquivos = { "cdtup.csv", "berco.csv", "portoatracacao.csv", "mes.csv", "tipooperacao.csv",
+        "tiponavegacaoatracacao.csv", "terminal.csv", "origem.csv", "destino.csv", "naturezacarga.csv", "sentido.csv" };
     
+
     mainDataset.open("dataset_00_1000_sem_virg.csv", fstream::in);
 
     if (mainDataset.is_open() == false) {
@@ -32,9 +65,7 @@ int main(int argc, char* argv[])
     }
    
 
-    vector<string> nomesArquivos = { "cdtup.csv", "berco.csv", "portoatracacao.csv", "mes.csv", "tipooperacao.csv", 
-        "tiponavegacaoatracacao.csv", "terminal.csv", "origem.csv", "destino.csv", "naturezacarga.csv", "sentido.csv" };
-
+    criarMapComNomeDaColunaAndPosicao();
     
     int NUM_LINHAS_LIDAS = 0;
    
@@ -53,13 +84,14 @@ int main(int argc, char* argv[])
 
         #pragma omp barrier 
 
-        #pragma omp parallel for
-        
+        #pragma omp for nowait
             for (int i = 0; i < nomesArquivos.size(); ++i) {
-                
+                printf("%s\n", matrizDeDados[1][idxColuna[nomesArquivos[i]]].c_str());
                 pairCodigoDescricao(nomesArquivos[i]);
           
             }
+        
+        #pragma omp barrier
     }
 
 
@@ -71,12 +103,29 @@ int main(int argc, char* argv[])
     std::cout << "Tempo       : " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "ms" << endl;    // Ending of parallel region 
 
 }
-//  ler o dataset csv e inserir em uma matriz, ele retorna o número de linhas que leu;
+/*
+    Esse map de consulta para saber o nome da coluna e o index que está associado.
+    esse map é utilizado na função pairCodigoDescricao()
+*/
+void criarMapComNomeDaColunaAndPosicao() {
+    vector<int> numeroDaColuna = { 1, 2, 3, 5, 6, 7, 8, 17, 18, 20, 23 };
+    int numColum = 0;
+    for (int i = 0; i < nomesArquivos.size(); ++i) {
+        idxColuna.insert(pair<string, int>(nomesArquivos[i], numeroDaColuna[numColum]));
+        numColum++;
+    }
 
+}
+
+//  ler o dataset csv e inserir em uma matriz, ele retorna o número de linhas que leu;
 int atualizarDataSet() {
     vector<string> dadosLocais;
+    
     int numLinhas;
-    for (numLinhas = 0; numLinhas < 500; numLinhas++) {
+    // NUMERO DE LINHAS DA MATRIZ: escolhido de forma árbitria podendo aumentar ou diminuir
+    const int NUM_DE_LINHAS_DA_MATRIZ = 500;
+
+    for (numLinhas = 0; numLinhas < NUM_DE_LINHAS_DA_MATRIZ; numLinhas++) {
         for (int i = 0; i < 25; i++) {
             string dado;
             getline(mainDataset, dado, ',');
@@ -94,6 +143,7 @@ int atualizarDataSet() {
     return numLinhas;
 }
 
+// Escreve o datasetFinal toda vez que a matriz é atualizada
 void escrita_do_dataset(vector<vector<string>> escritaMatriz) {
     
     int NUM_LINHAS = escritaMatriz.size();
@@ -116,7 +166,7 @@ void pairCodigoDescricao(string nomeArquivo) {
 
     std::fstream arquivo;
     arquivo.open(nomeArquivo, fstream::in | fstream::out | fstream::app);
-
+    int NUM_COLUM = idxColuna[nomeArquivo];
 
     if (arquivo.is_open()) {
         
